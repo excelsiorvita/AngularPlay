01d0ea3cb313269b840c5877bad0ee913f5c0f01
excelsiorvita <lesbuble0707@gmail.com>
2023-10-11 16:45:22 +0300

    Added a recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe','This is a test','https://www.cookipedia.co.uk/wiki/images/7/7d/Aperitif_cheese_balls_recipe.jpg')
+  ];
 
+  constructor(){}
+  ngOnInit() {
+      
+  }
 }
